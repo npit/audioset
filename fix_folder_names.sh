61685da3f23244ef9908529c8ac2d55e5e7ee2a7
npit <pittarasnikif@gmail.com>
2018-02-22 18:26:03 +0200

    Suffix param to folder fixer

--- fix_folder_names.sh
+++ fix_folder_names.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 folder="$1"
+suff="$2"
+simulate=0
+[ $# -gt 2 ] && [ "$3" == "simulate" ] && simulate=1
 for vidfolder in $(ls $folder); do
-	mv "$folder/$vidfolder" "$folder/$( echo $vidfolder | sed 's<\_[0-9]*\_[0-9]*\.mp4<<g')"
+	if [ $simulate -eq 1 ]; then
+		echo "(simulating)"
+		echo "$folder/$vidfolder" 
+		echo "$folder/$( echo $vidfolder | sed 's<\_[0-9]*\_[0-9]*<<g')"
+	else
+		mv "$folder/$vidfolder" "$folder/$( echo $vidfolder | sed 's<\_[0-9]*\_[0-9]*<<g')"
+	fi
 done
